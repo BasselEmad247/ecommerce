891b2077aab1f15c36d78fe23e15847c0d9f3c47
BasselEmad247 <BasselEmad247@gmail.com>
2021-09-27 18:03:59 +0200

    Project Initiation

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "ecommerce",
+    home: Scaffold(
+      body: Container(),
+    ),
+    debugShowCheckedModeBanner: false,
+  ));
+}
