d2b672f35a4a9e0e57e3939bed9f4d9215b621a8
BasselEmad247 <BasselEmad247@gmail.com>
2021-10-02 15:13:48 +0200

    Home screen static design

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/widgets/item.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,6 +6,33 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.grey[400],
+      child: ListView(
+        children: [
+          Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Item("https://thumbs.dreamstime.com/b/phone-icon-black-white-telephone-symbol-vector-illustration-black-handset-icon-white-background-vector-illustration-132728333.jpg", "fdfs", "fds", 123),
+            Item("https://thumbs.dreamstime.com/b/phone-icon-black-white-telephone-symbol-vector-illustration-black-handset-icon-white-background-vector-illustration-132728333.jpg", "fdfs", "fds", 123),
+          ],
+        ),
+          Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Item("https://thumbs.dreamstime.com/b/phone-icon-black-white-telephone-symbol-vector-illustration-black-handset-icon-white-background-vector-illustration-132728333.jpg", "fdfs", "fds", 123),
+            Item("https://thumbs.dreamstime.com/b/phone-icon-black-white-telephone-symbol-vector-illustration-black-handset-icon-white-background-vector-illustration-132728333.jpg", "fdfs", "fds", 123),
+          ],
+        ),
+          Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Item("https://thumbs.dreamstime.com/b/phone-icon-black-white-telephone-symbol-vector-illustration-black-handset-icon-white-background-vector-illustration-132728333.jpg", "fdfs", "fds", 123),
+            Item("https://thumbs.dreamstime.com/b/phone-icon-black-white-telephone-symbol-vector-illustration-black-handset-icon-white-background-vector-illustration-132728333.jpg", "fdfs", "fds", 123),
+          ],
+        ),
+        ]
+      ),
+    );
   }
 }
