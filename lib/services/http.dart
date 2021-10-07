6e4166102b9d8d236337bef379169985f3efbf39
BasselEmad247 <BasselEmad247@gmail.com>
2021-10-08 00:46:55 +0200

    Added more apis to http service

--- lib/services/http.dart
+++ lib/services/http.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
+import 'package:ecommerce/models/category.dart';
 import 'package:ecommerce/models/item.dart';
 import 'package:http/http.dart' as http;
 
 class Http {
-  String allItems = "https://retail.amit-learning.com/api/products";
+  String allItems = "https://retail.amit-learning.com/api/products/";
+  String allCategories = "https://retail.amit-learning.com/api/categories/";
 
   Future<List<Item>> fetchItems() async {
     final response = await http.get(Uri.parse(allItems));
@@ -17,6 +19,48 @@ class Http {
       return items;
     } else {
       throw Exception('Failed to load items');
+    }
+  }
+
+  Future<List<Category>> fetchCategories() async {
+    final response = await http.get(Uri.parse(allCategories));
+
+    if (response.statusCode == 200) {
+      List<dynamic> categoriesJson = jsonDecode(response.body)["categories"];
+
+      List<Category> categories = categoriesModel(categoriesJson);
+
+      return categories;
+    } else {
+      throw Exception('Failed to load categories');
+    }
+  }
+
+  Future<Item> fetchItem(int id) async {
+    final response = await http.get(Uri.parse(allItems + id.toString()));
+
+    if (response.statusCode == 200) {
+      dynamic itemJson = jsonDecode(response.body)["product"];
+
+      Item item = itemModel(itemJson);
+
+      return item;
+    } else {
+      throw Exception('Failed to load item');
+    }
+  }
+
+  Future<Category> fetchCategory(int id) async {
+    final response = await http.get(Uri.parse(allCategories + id.toString()));
+
+    if (response.statusCode == 200) {
+      dynamic categoryJson = jsonDecode(response.body)["category"];
+
+      Category category = categoryModel(categoryJson);
+
+      return category;
+    } else {
+      throw Exception('Failed to load category');
     }
   }
 
@@ -49,5 +93,47 @@ class Http {
       items.add(item);
     }
     return items;
+  }
+
+  List<Category> categoriesModel(List<dynamic> categoriesJson) {
+    List<Category> categories = [];
+
+    for (dynamic i in categoriesJson) {
+      if (i["id"] == null || i["name"] == null || i["avatar"] == null) {
+        continue;
+      }
+      Category category =
+          Category(id: i["id"], name: i["name"], avatar: i["avatar"]);
+
+      categories.add(category);
+    }
+    return categories;
+  }
+
+  Item itemModel(dynamic itemJson) {
+    Item item = Item(
+        id: itemJson["id"],
+        name: itemJson["name"],
+        title: itemJson["title"],
+        category_id: itemJson["category_id"],
+        description: itemJson["description"],
+        price: itemJson["price"],
+        discount: itemJson["discount"],
+        discount_type: itemJson["discount_type"],
+        currency: itemJson["currency"],
+        in_stock: itemJson["in_stock"],
+        avatar: itemJson["avatar"],
+        price_final: itemJson["price_final"],
+        price_final_text: itemJson["price_final_text"]);
+
+    return item;
+  }
+
+  Category categoryModel(dynamic categoryJson) {
+    Category category = Category(
+        id: categoryJson["id"],
+        name: categoryJson["name"],
+        avatar: categoryJson["avatar"]);
+    return category;
   }
 }
