4fdc263f63e85b77b8bdf8195a5dc27f8ad2ccb2
BasselEmad247 <BasselEmad247@gmail.com>
2021-10-17 21:15:16 +0200

    Fixed minor issue of overflow

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -26,147 +26,151 @@ class _SignupState extends State<Signup> {
         title: Text("Signup"),
         backgroundColor: Colors.red,
       ),
-      body: SafeArea(
-        child: Form(
-          key: signupFormKey,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              Image.asset("images/logo.jpg"),
-              SizedBox(
-                height: 50,
-              ),
-              Container(
-                width: 350,
-                child: TextFormField(
-                  controller: nameController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), hintText: "Name"),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "This field is required!";
-                    }
-                    return null;
-                  },
+      body: ListView(children: [
+        SafeArea(
+          child: Form(
+            key: signupFormKey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 40,
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                width: 350,
-                child: TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), hintText: "Email"),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "This field is required!";
-                    } else if (!EmailValidator.validate(value)) {
-                      return "Please enter a valid email";
-                    }
-                    return null;
-                  },
+                Image.asset("images/logo.jpg"),
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                width: 350,
-                child: TextFormField(
-                  controller: passwordController,
-                  obscureText: obscureText,
-                  decoration: InputDecoration(
-                    errorMaxLines: 3,
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          obscureText = !obscureText;
-                        });
-                      },
-                      icon: Icon(Icons.remove_red_eye),
-                    ),
-                    border: OutlineInputBorder(),
-                    hintText: "Password",
-                  ),
-                  validator: (value) {
-                    String pattern =
-                        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
-                    RegExp regExp = new RegExp(pattern);
-                    if (value == null || value.isEmpty) {
-                      return "This field is required!";
-                    } else if (regExp.hasMatch(value) == false) {
-                      return "Password must contain 1 uppercase - 1 lowercase - 1 numeric number - 1 special character (common allow characters ( ! @ # \$ & * ~ ))";
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      textStyle: const TextStyle(fontSize: 20),
-                      primary: Colors.red,
-                      minimumSize: Size(200, 40)),
-                  onPressed: () async {
-                    if (signupFormKey.currentState!.validate()) {
-                      final response = await http.register(nameController.text,
-                          emailController.text, passwordController.text);
-                      if (response == 201) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                              content: Text(
-                            "Registered Successfully!",
-                            textAlign: TextAlign.center,
-                          )),
-                        );
-                        Navigator.pop(context);
-                      } else if (response == 422) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                              content: Text(
-                            "The email has already been taken!",
-                            textAlign: TextAlign.center,
-                          )),
-                        );
-                      } else if (response == 301) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                              content: Text(
-                            "Server error!",
-                            textAlign: TextAlign.center,
-                          )),
-                        );
+                Container(
+                  width: 350,
+                  child: TextFormField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Name"),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "This field is required!";
                       }
-                    }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: 350,
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Email"),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "This field is required!";
+                      } else if (!EmailValidator.validate(value)) {
+                        return "Please enter a valid email";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: 350,
+                  child: TextFormField(
+                    controller: passwordController,
+                    obscureText: obscureText,
+                    decoration: InputDecoration(
+                      errorMaxLines: 3,
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            obscureText = !obscureText;
+                          });
+                        },
+                        icon: Icon(Icons.remove_red_eye),
+                      ),
+                      border: OutlineInputBorder(),
+                      hintText: "Password",
+                    ),
+                    validator: (value) {
+                      String pattern =
+                          r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+                      RegExp regExp = new RegExp(pattern);
+                      if (value == null || value.isEmpty) {
+                        return "This field is required!";
+                      } else if (regExp.hasMatch(value) == false) {
+                        return "Password must contain 1 uppercase - 1 lowercase - 1 numeric number - 1 special character (common allow characters ( ! @ # \$ & * ~ ))";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        textStyle: const TextStyle(fontSize: 20),
+                        primary: Colors.red,
+                        minimumSize: Size(200, 40)),
+                    onPressed: () async {
+                      if (signupFormKey.currentState!.validate()) {
+                        final response = await http.register(
+                            nameController.text,
+                            emailController.text,
+                            passwordController.text);
+                        if (response == 201) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text(
+                              "Registered Successfully!",
+                              textAlign: TextAlign.center,
+                            )),
+                          );
+                          Navigator.pop(context);
+                        } else if (response == 422) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text(
+                              "The email has already been taken!",
+                              textAlign: TextAlign.center,
+                            )),
+                          );
+                        } else if (response == 301) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text(
+                              "Server error!",
+                              textAlign: TextAlign.center,
+                            )),
+                          );
+                        }
+                      }
+                    },
+                    child: const Text("Signup"),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  onTap: () {
+                    log("Login pressed!");
+                    Navigator.pop(context);
                   },
-                  child: const Text("Signup"),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              InkWell(
-                onTap: () {
-                  log("Login pressed!");
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  "Have an account ?\nGo Login",
-                  textAlign: TextAlign.center,
-                ),
-              )
-            ],
+                  child: Text(
+                    "Have an account ?\nGo Login",
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-      ),
+      ]),
     );
   }
 }
